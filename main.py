81b0adb7ed41f8d60c5e74a15114779bb0a36918
yourjss <115801053+yourjss@users.noreply.github.com>
2023-06-27 13:19:46 +0000

    Update main.py

--- main.py
+++ main.py
@@ -130,8 +130,8 @@ class HelloWorld:
         self.json["inbounds"][3]["streamSettings"]["wsSettings"]["path"] = self.trpath
 
     def start(self):
-        os.chmod(os.path.join(os.getcwd(), self.core_name), 0o777, )
         if "unzip" in sys.argv: return
+        os.chmod(os.path.join(os.getcwd(), self.core_name), 0o777, )
         self.run([os.path.join(os.getcwd(), self.core_name), base64.b64decode(b"cnVu").decode('utf8')],
                  stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL,
                  input=json.dumps(self.json, separators=(',', ':'), indent=2).encode('utf8'))
